e5898b3b40bfbc5572e1b0a2601e819b49a3efed
Nguyễn Phước Vinh <npvinh2410@gmail.com>
2024-02-24 16:25:43 +0700

    add function for task manager

--- task_manager/app.js
+++ task_manager/app.js
@@ -1,12 +1,34 @@
-const express = require('express');
-const app = express();
+
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+const notFound = require('./middleware/not-found')
+
+// midleware
+
+app.use(express.json())
+app.use(express.static('./public'))
 
 // routes
-app.get('/hello', (req, res) => {
 
-    res.send('Hello World!');
-});
+
+app.use('/api/v1/tasks',tasks)
+
+
+app.use(notFound)
+
 
 const port = 3000
 
-app.listen(port, console.log(`Server listening on ${port}`));
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server listening on ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
