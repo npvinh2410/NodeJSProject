9ef1bb69b9e3a0074a0687738dc64e88d69c46d5
Nguyễn Phước Vinh <npvinh2410@gmail.com>
2024-02-25 21:59:44 +0700

    add JWT Basics project

--- task_manager/app.js
+++ task_manager/app.js
@@ -5,7 +5,7 @@ const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
 const notFound = require('./middleware/not-found')
-
+const errorHandlerMiddleware = require('./middleware/error-handler')
 // midleware
 
 app.use(express.json())
@@ -20,7 +20,7 @@ app.use('/api/v1/tasks',tasks)
 app.use(notFound)
 
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 const start = async () => {
     try {
